9a690fc80fa4ff79428986539f5ef4bd7064ceb2
Francis Galiegue <fgaliegue@gmail.com>
2015-12-11 02:07:37 +0100

    First try at an implementation of BasicFileAttributes over Apache's commons-net
    Apache's commons-net provides an FTPFile class, use that.

---
+++ java7-fs-ftp/src/main/java/com/github/fge/fs/ftp/attr/attributes/FtpBasicFileAttributes.java
@@ -0,0 +1,41 @@
+package com.github.fge.fs.ftp.attr.attributes;
+
+import com.github.fge.fs.api.attr.attributes.AbstractBasicFileAttributes;
+import org.apache.commons.net.ftp.FTPFile;
+
+import java.nio.file.attribute.FileTime;
+
+public final class FtpBasicFileAttributes
+    extends AbstractBasicFileAttributes
+{
+    private final FTPFile ftpFile;
+
+    public FtpBasicFileAttributes(final FTPFile file)
+    {
+        ftpFile = file;
+    }
+
+    @Override
+    public FileTime lastModifiedTime()
+    {
+        return FileTime.from(ftpFile.getTimestamp().toInstant());
+    }
+
+    @Override
+    public boolean isRegularFile()
+    {
+        return ftpFile.isFile();
+    }
+
+    @Override
+    public boolean isDirectory()
+    {
+        return ftpFile.isDirectory();
+    }
+
+    @Override
+    public long size()
+    {
+        return ftpFile.getSize();
+    }
+}
